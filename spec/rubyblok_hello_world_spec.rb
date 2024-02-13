b922be6bd57298d9c6808e62db8bd0dfb76926db
Gabriel Soares <gabrielsods@live.com>
2024-02-13 06:43:08 -0300

    [RB-133] Extracting the storyblok client service into the gem

---
+++ spec/rubyblok_hello_world_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/rubyblok/hello_world'
+
+describe Rubyblok::Hello do
+  let(:rubyblok) { described_class.new }
+
+  it "say_hello" do
+    expect(rubyblok.say_hello).to eq("hello world")
+  end
+end
