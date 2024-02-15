11d69e30a5b872ecfb61d7af837862ec01f7ab17
Gabriel Soares <gabrielsods@live.com>
2024-02-15 19:56:30 -0300

    [RB-47] - Remove unnecessary spec

--- spec/rubyblok_hello_world_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/rubyblok/hello_world'
-
-describe Rubyblok::Hello do
-  let(:rubyblok) { described_class.new }
-
-  it "say_hello" do
-    expect(rubyblok.say_hello).to eq("hello world")
-  end
-end
