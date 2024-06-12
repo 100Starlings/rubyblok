a62826f450493a604549ce295fcf63e0a55daae8
Zoltan Iklodi <ziklodi@gmail.com>
2024-06-12 09:09:52 +0200

    B-220 caching views rendering
    It applies fragment view caching when rendering story content.

--- lib/rubyblok/configuration.rb
+++ lib/rubyblok/configuration.rb
@@ -2,7 +2,7 @@
 
 module Rubyblok
   class Configuration
-    attr_accessor :api_token, :auto_update, :cached, :use_cdn_images, :component_path, :image_model_name,
-                  :model_name, :version, :webhook_secret
+    attr_accessor :api_token, :auto_update, :cached, :cache_views, :component_path, :image_model_name,
+                  :model_name, :use_cdn_images, :version, :webhook_secret
   end
 end
